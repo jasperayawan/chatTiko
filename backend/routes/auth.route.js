ec3d806d024e6b6c2ff93160a3349dfa1b29d1bd
Jasper Ayawan <ejayawan22@gmail.com>
2024-05-28 21:28:18 +0800

    update controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { signup, login, logout } = require('../Controller/Auth.controller');
+const { signup, login, logout } = require('../controller/Auth.controller')
 
 router.post("/signup", signup)
 router.post("/login", login)
