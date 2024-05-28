2594d8ea0dc12b1e4982ae26035bbbce0bc5be0b
Jasper Ayawan <ejayawan22@gmail.com>
2024-05-28 20:53:55 +0800

    update backend

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
+
 server.listen(port, () => {
     console.log("server listening to port:", port)
     connectToMongoDb()
